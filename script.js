f141275fb81d9cc503c8f06c6cf906c5aa9e87ff
Caglar Guler <caglarguler626262@gmail.com>
2020-10-07 18:58:06 -0400

    create animal age conventor

---
+++ script.js
@@ -0,0 +1,11 @@
+let myAge = 32;
+let earlyYears = 2;
+earlyYears *= 10.5;
+myAge = myAge - 2;
+let laterYears = myAge;
+laterYears *= 4;
+let myAgeInDogYears = earlyYears + laterYears;
+let name = 'Caglar';
+let myName = name .toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
